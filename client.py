56e4b92548c6e4952957bc3366d683d7e78457e6
Kevin Fangzhou Chen <kevinchen1219@gmail.com>
2016-04-08 11:55:31 -0700

    local t14 passing, but not autograder

--- client.py
+++ client.py
@@ -48,12 +48,17 @@ class Client(BaseClient):
             k_a = info["files_I_own"][name]["k_a"]
             r = info["files_I_own"][name]["r"]
             username = self.username
+
         elif name in info["files_shared_to_me"]:
-            k_e = info["files_shared_to_me"][name]["k_e"]
-            k_a = info["files_shared_to_me"][name]["k_a"]
-            r = info["files_shared_to_me"][name]["r"]
-            username = info["files_shared_to_me"][name]["from_user"]
+            link = info["files_shared_to_me"][name][0]
+            from_username = info["files_shared_to_me"][name][1]
+            secret = self.get_linked_data(link, from_username)
+            k_e = secret["k_e"]
+            k_a = secret["k_a"]
+            r = secret["r"]
+            username = secret["from_user"]
         else:
+            # first time uploading a file
             k_e = self.crypto.get_random_bytes(16)
             k_a = self.crypto.get_random_bytes(16)
             k_n = self.crypto.get_random_bytes(16)
@@ -95,11 +100,15 @@ class Client(BaseClient):
             k_a = info["files_I_own"][name]["k_a"]
             r = info["files_I_own"][name]["r"]
             username = self.username
+
         elif name in info["files_shared_to_me"]:
-            k_e = info["files_shared_to_me"][name]["k_e"]
-            k_a = info["files_shared_to_me"][name]["k_a"]
-            r = info["files_shared_to_me"][name]["r"]
-            username = info["files_shared_to_me"][name]["from_user"]
+            link = info["files_shared_to_me"][name][0]
+            from_username = info["files_shared_to_me"][name][1]
+            secret = self.get_linked_data(link, from_username)
+            k_e = secret["k_e"]
+            k_a = secret["k_a"]
+            r = secret["r"]
+            username = secret["from_user"]
         else:
             return None
 
@@ -148,39 +157,43 @@ class Client(BaseClient):
             else:
                 raise ValueError("You have already shared this file to this person.")
             self.update_information(info)
+
         elif name in info["files_shared_to_me"]:
-            k_e = info["files_shared_to_me"][name]["k_e"]
-            k_a = info["files_shared_to_me"][name]["k_a"]
-            r = info["files_shared_to_me"][name]["r"]
-            username = info["files_shared_to_me"][name]["from_user"]
+            infolink = info["files_shared_to_me"][name][0]
+            from_username = info["files_shared_to_me"][name][1]
+            secret = self.get_linked_data(infolink, from_username)
+            k_e = secret["k_e"]
+            k_a = secret["k_a"]
+            r = secret["r"]
+            username = secret["from_user"]
+
         else:
-            return
+            return None
 
         secret = {"k_e": k_e,
                   "k_a": k_a,
                   "r": r,
                   "from_user": username}
-        secret_string = to_json_string(secret)
-        encrypted_secret = self.crypto.asymmetric_encrypt(secret_string, self.pks.get_public_key(user))
-        self.storage_server.put(path_join(self.username, "shared", user, link),
-                                encrypted_secret)
+        linked_data = self.generate_linked_data(secret, user)
+
+        self.storage_server.put(path_join(user, "shared", link),
+                                to_json_string(linked_data))
         signed_link = self.crypto.asymmetric_sign(link, self.private_key)
         return link, signed_link
 
     def receive_share(self, from_username, newname, message):
-        # Replace with your implementation (not needed for Part 1)
+        if message is None:
+            raise ValueError("You received empty message")
         link = message[0]
         link_signature = message[1]
         verify = self.crypto.asymmetric_verify(link, link_signature, self.pks.get_public_key(from_username))
         if not verify:
             raise IntegrityError("Link is modified during transmission, don't click")
 
-        encrypted_secret = self.storage_server.get(path_join(from_username, "shared", self.username, link))
-        secret_string = self.crypto.asymmetric_decrypt(encrypted_secret, self.private_key)
-        secret = from_json_string(secret_string)
-        # secret["from_username"] = from_username
         info = self.get_information()
-        info["files_shared_to_me"][newname] = secret
+        info["files_shared_to_me"][newname] = (link, from_username)
+                                                     # So that we know where the link come from,
+                                                     # so that we know whose public key we should use to verify
         self.update_information(info)
 
     def revoke(self, user, name):
@@ -195,19 +208,28 @@ class Client(BaseClient):
         r = self.crypto.cryptographic_hash(name_key, hash_name='SHA256')
         users = info["files_I_own"][name]["users"]
 
+        to_remove = []
+
         for person in users:
             if person[0] != user:
+                gooduser = person[0]
+                gooduser_link = person[1]
+
                 secret = {"k_e": new_k_e,
                           "k_a": new_k_a,
                           "r": r,
                           "from_user": self.username}
-                secret_string = to_json_string(secret)
-                encrypted_secret = self.crypto.asymmetric_encrypt(secret_string,
-                                                                  self.pks.get_public_key(person[0]))
-                self.storage_server.put(path_join(self.username, "shared", person[0], person[1]),
-                                        encrypted_secret)
+
+                linked_data = self.generate_linked_data(secret, gooduser)
+                self.storage_server.put(path_join(gooduser, "shared", gooduser_link),
+                                        to_json_string(linked_data))
+
             else:
-                users.remove(person)
+                to_remove.append(person)
+
+        while to_remove:
+            person = to_remove.pop()
+            users.remove(person)
 
         info["files_I_own"][name] = {
                     "users": users,  # using list, maybe run time is slow when lookup?
@@ -219,6 +241,36 @@ class Client(BaseClient):
         data = self.download(name)
         self.update_information(info)
         self.upload(name, data)
+
+    def update_information(self, new_info):
+        """
+        Encrypt and put on server the updated info, and update the signature as well.
+
+        :param new_info: updated info, a dictionary
+        :return: nothing
+        """
+        symmetric_key = self.crypto.get_random_bytes(16)
+        public_key = self.private_key.publickey()
+        info_path = path_join("information", self.username)
+
+        new_info_string = to_json_string(new_info)
+        signature_path = path_join("information", self.username, "signature")
+
+        crypto_iv = self.crypto.get_random_bytes(16)
+        encrypted_info = self.crypto.symmetric_encrypt(new_info_string, symmetric_key,
+                                                       cipher_name='AES',
+                                                       mode_name='CBC',
+                                                       iv=crypto_iv)
+        encrypted_k = self.crypto.asymmetric_encrypt(symmetric_key, public_key)
+
+        cipher_text = to_json_string({"encrypted_k": encrypted_k,
+                                      "crypto_iv":  crypto_iv,
+                                      "encrypted_info": encrypted_info})
+
+        info_signature = self.crypto.asymmetric_sign(cipher_text, self.private_key)
+
+        self.storage_server.put(info_path, cipher_text)
+        self.storage_server.put(signature_path, info_signature)
 
     def get_information(self):
         """
@@ -240,43 +292,69 @@ class Client(BaseClient):
         k = self.crypto.asymmetric_decrypt(encrypted_info["encrypted_k"], private_key)
         iv = encrypted_info["crypto_iv"]
         data = encrypted_info["encrypted_info"]
-        info = self.crypto.symmetric_decrypt(data,
-                                             k,
-                                             cipher_name='AES',
-                                             mode_name='CBC',
-                                             iv=iv)
-        info = from_json_string(info)
+        try:
+            info = self.crypto.symmetric_decrypt(data,
+                                                 k,
+                                                 cipher_name='AES',
+                                                 mode_name='CBC',
+                                                 iv=iv)
+            return from_json_string(info)
+        except ValueError:
+            raise IntegrityError("symmetric decryption failed during getting meta-info about a user, IV?")
 
-        return info
-
-    def update_information(self, new_info):
+    def generate_linked_data(self, secret, target_user):
         """
-        Encrypt and put on server the updated info, and update the signature as well.
+        Generate linked data
+
+        :param
+        :return linked_data
+        """
+        crypto_iv = self.crypto.get_random_bytes(16)
+        symmetric_key = self.crypto.get_random_bytes(16)
+
+        encrypted_secret = self.crypto.symmetric_encrypt(to_json_string(secret), symmetric_key,
+                                                         cipher_name='AES',
+                                                         mode_name="CBC",
+                                                         iv=crypto_iv)
+        encrypted_key = self.crypto.asymmetric_encrypt(symmetric_key, self.pks.get_public_key(target_user))
+        signature = self.crypto.asymmetric_sign(encrypted_secret + encrypted_key + crypto_iv, self.private_key)
+        linked_data = {"encrypted_secret": encrypted_secret,
+                       "encrypted_key": encrypted_key,
+                       "iv": crypto_iv,
+                       "signature": signature}
+
+        return linked_data
+
+
+    def get_linked_data(self, link, link_fromuser):
+        """
+        Get linked data, and validate its integrity,
 
         :param new_info: updated info, a dictionary
-        :return: nothing
+        :return: secret, only if the integrity is not violated
         """
-        symmetric_key = self.crypto.get_random_bytes(16)
-        public_key = self.private_key.publickey()
-        info_path = path_join("information", self.username)
 
-        new_info_string = to_json_string(new_info)
-        signature_path = path_join("information", self.username, "signature")
+        linked_data = self.storage_server.get(path_join(self.username, "shared", link))
+        linked_data = from_json_string(linked_data)
 
-        crypto_iv = self.crypto.get_random_bytes(16)
+        encrypted_secret = linked_data["encrypted_secret"]
+        encrypted_key = linked_data["encrypted_key"]
+        signature = linked_data["signature"]
+        crypto_iv = linked_data["iv"]
+        verify = self.crypto.asymmetric_verify(encrypted_secret + encrypted_key + crypto_iv, signature,
+                                               self.pks.get_public_key(link_fromuser))
+        if not verify:
+            raise IntegrityError("linked data has been compromised")
 
-        encrypted_info = self.crypto.symmetric_encrypt(new_info_string, symmetric_key,
-                                                       cipher_name='AES',
-                                                       mode_name='CBC',
-                                                       iv=crypto_iv)
-        encrypted_k = self.crypto.asymmetric_encrypt(symmetric_key, public_key)
+        symmetric_key = self.crypto.asymmetric_decrypt(encrypted_key, self.private_key)
+        try:
+            secret = from_json_string(self.crypto.symmetric_decrypt(encrypted_secret,
+                                                                    symmetric_key,
+                                                                    cipher_name='AES',
+                                                                    mode_name='CBC',
+                                                                    iv=crypto_iv))
+            return secret
+        except ValueError:
+            raise IntegrityError("symmetric decryption failed during getting linked data, IV messed up?")
 
-        cipher_text = to_json_string({"encrypted_k": encrypted_k,
-                                      "crypto_iv":  crypto_iv,
-                                      "encrypted_info": encrypted_info})
-
-        signed_info = self.crypto.asymmetric_sign(cipher_text, self.private_key)
-
-        self.storage_server.put(info_path, cipher_text)
-        self.storage_server.put(signature_path, signed_info)
 
